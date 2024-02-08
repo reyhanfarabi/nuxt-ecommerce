5dca0ae3f47db5644771d5480e681e2014b00b93
Reyhan Farabi <64721326+reyhanfarabi@users.noreply.github.com>
2024-02-08 20:10:08 +0700

    remove navmenu

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,12 +1,5 @@
 <template>
-  <header class="flex flex-row w-full justify-between px-10">
-    <ul class="flex flex-row items-center gap-8">
-      <li><NuxtLink class="nav-link active" to="#"> All </NuxtLink></li>
-      <li><NuxtLink class="nav-link" to="#"> Electronics </NuxtLink></li>
-      <li><NuxtLink class="nav-link" to="#"> Jewelry </NuxtLink></li>
-      <li><NuxtLink class="nav-link" to="#"> Men's Clothing </NuxtLink></li>
-      <li><NuxtLink class="nav-link" to="#"> Women's Clothing </NuxtLink></li>
-    </ul>
+  <header class="flex flex-row w-full justify-end px-10">
     <h1 class="px-8 py-6 text-white bg-zinc-950">E-Commerce</h1>
   </header>
 </template>
