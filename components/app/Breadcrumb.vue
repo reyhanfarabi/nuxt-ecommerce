d581324c1b3c8aca29882a7cc46e43648bdfe7b0
Reyhan Farabi <64721326+reyhanfarabi@users.noreply.github.com>
2024-02-08 22:45:47 +0700

    add breacrumb to product detail page
    create breadcrumb component
    add breacrumb component to product detail page

---
+++ components/app/Breadcrumb.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="flex flex-row gap-2 text-sm">
+    <li v-for="(link, index) in props.links" class="flex flex-row gap-2">
+      <NuxtLink
+        :to="link.to"
+        :class="[{ 'font-semibold': index === props.links.length - 1 }]"
+      >
+        <span>{{ link.label }}</span>
+      </NuxtLink>
+      <span v-if="index !== props.links.length - 1">/</span>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+export interface IBreadcrumbItem {
+  label: string;
+  to?: string;
+}
+
+const props = defineProps<{
+  links: Array<IBreadcrumbItem>;
+}>();
+</script>
--- pages/[id].vue
+++ pages/[id].vue
@@ -1,37 +1,42 @@
 <template>
   <Title>{{ `${product?.title} - Product Detail` || "Product Detail" }}</Title>
   <div class="flex flex-col items-center w-full">
-    <div class="flex flex-row container px-36 gap-10">
-      <div
-        class="flex justify-center items-center h-96 w-96 p-4 border border-gray-200 rounded-md"
-      >
-        <img
-          class="h-full"
-          :src="product?.image"
-          :alt="`${product?.title} product image`"
-        />
-      </div>
-      <div class="flex flex-1 flex-col gap-2">
-        <span class="text-lg">{{ toCapitalize(product?.category || "") }}</span>
-        <div class="flex flex-col gap-4">
-          <h1 class="text-4xl">{{ product?.title }}</h1>
-          <span
-            >{{ product?.rating.rate.toFixed(1) }} ({{
-              product?.rating.count
-            }}
-            ratings)</span
-          >
-          <span class="text-2xl font-bold">${{ product?.price }}</span>
-          <div class="flex flex-col gap-2 border border-zinc-950 p-4">
-            <label class="text-sm font-bold" for="description-box"
-              >Description</label
-            >
-            <p name="description-box">{{ product?.description }}</p>
-          </div>
+    <div class="flex flex-col container gap-4 px-36">
+      <AppBreadcrumb :links="breadcrumbLinks" />
+      <div class="flex flex-row gap-10">
+        <div
+          class="flex justify-center items-center h-96 w-96 p-4 border border-gray-200 rounded-md"
+        >
+          <img
+            class="h-full"
+            :src="product?.image"
+            :alt="`${product?.title} product image`"
+          />
         </div>
-        <div class="flex justify-end items-end h-full gap-4 pt-8">
-          <AppButton> Add to Cart </AppButton>
-          <AppButton is-filled>Buy</AppButton>
+        <div class="flex flex-1 flex-col gap-2">
+          <span class="text-lg">{{
+            toCapitalize(product?.category || "")
+          }}</span>
+          <div class="flex flex-col gap-4">
+            <h1 class="text-4xl">{{ product?.title }}</h1>
+            <span
+              >{{ product?.rating.rate.toFixed(1) }} ({{
+                product?.rating.count
+              }}
+              ratings)</span
+            >
+            <span class="text-2xl font-bold">${{ product?.price }}</span>
+            <div class="flex flex-col gap-2 border border-zinc-950 p-4">
+              <label class="text-sm font-bold" for="description-box"
+                >Description</label
+              >
+              <p name="description-box">{{ product?.description }}</p>
+            </div>
+          </div>
+          <div class="flex justify-end items-end h-full gap-4 pt-8">
+            <AppButton> Add to Cart </AppButton>
+            <AppButton is-filled>Buy</AppButton>
+          </div>
         </div>
       </div>
     </div>
@@ -39,6 +44,7 @@
 </template>
 
 <script lang="ts" setup>
+import type { IBreadcrumbItem } from "~/components/app/Breadcrumb.vue";
 import type { IProduct } from "~/types/product";
 
 const config = useRuntimeConfig();
@@ -47,4 +53,9 @@ const route = useRoute();
 const { data: product } = await useFetch<IProduct>(
   `${config.public.apiUrl}/products/${route.params.id}`
 );
+
+const breadcrumbLinks: Array<IBreadcrumbItem> = [
+  { label: "Products", to: "/" },
+  { label: product.value?.title || "" },
+];
 </script>
